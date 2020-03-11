d440bb0a8577968b4aa418bb4bd09d1da65e25db
Gab2904 <gabriel.r.martins@outlook.com>
2020-03-10 21:56:06 -0300

    Versão 1.1 - Classe Aluno - Get e Set

--- Aluno.java
+++ Aluno.java
@@ -3,5 +3,17 @@ public class Aluno {
 	
 	String nome;
 	int id;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
